27b9d284c8d737d9d29929fc7dc8b5a7af901637
Angel <angelshce@gmail.com>
2023-07-30 00:29:18 +0200

    Simplificado el modelo

--- CTOV2/models.py
+++ CTOV2/models.py
@@ -1,69 +1,58 @@
 from django.db import models
 
-class User(models.Model):
+from django.db import models
+
+class Users(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    mail = models.EmailField(max_length=255)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
     password = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     active = models.IntegerField()
 
-class Department(models.Model):
+class Teams(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    desc = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+
+class Departments(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
     active = models.IntegerField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
-class Project(models.Model):
+class Projects(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    desc = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
     status = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(Users, on_delete=models.CASCADE)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
-class Task(models.Model):
+class Tasks(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    desc = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
     status = models.CharField(max_length=255)
     start_date = models.DateField()
     end_date = models.DateField()
-    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by_tasks')
+    assigned_to = models.ForeignKey(Users, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='tasks_created_by')
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
-class Task_status(models.Model):
+class Comments(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
-
-
-class TaskComment(models.Model):
-    id = models.AutoField(primary_key=True)
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_type = models.CharField(max_length=255)
+    related_id = models.IntegerField()
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
     comment = models.TextField()
     created_at = models.DateTimeField()
-
-class DepartmentsUsers(models.Model):
-    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class ProjectsUsers(models.Model):
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-
-class ProjectsTasks(models.Model):
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
-    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
